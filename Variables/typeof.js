9acc4ea7f7bb1bc3af9beff9641d8131a36bc1c0
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-06 12:08:00 +0300

    типы данных

---
+++ Variables/typeof.js
@@ -0,0 +1,14 @@
+let name = 'Ravil';
+
+console.log(`hello ${1 + 8}`);
+console.log(`hello ${name}`);
+console.log(`hello ${'name'}`);
+
+console.log(typeof 5);
+console.log(typeof undefined);
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof 'hi');
+console.log(typeof Symbol('id'));
+console.log(typeof Math);
+console.log(typeof null);
