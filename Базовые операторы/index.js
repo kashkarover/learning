bc443b07e3c61a15508825ddcaf07d800196ac20
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-06 17:48:44 +0300

    commit

---
+++ Базовые операторы/index.js
@@ -0,0 +1,28 @@
+console.log(5 % 2);
+console.log(45 ** 6);
+console.log('--------------------------');
+
+console.log(1 + '2');
+console.log('1' + 2);
+console.log(100 / '2');
+console.log('--------------------------');
+
+console.log(+true);
+console.log(+false);
+console.log(+'');
+console.log(+null);
+console.log(+undefined);
+console.log(+NaN);
+console.log('--------------------------');
+
+let apples = '2';
+let oranges = '3';
+
+console.log(apples + oranges);
+console.log(+apples + +oranges);
+console.log('--------------------------');
+
+let counter = 0;
+let counter1 = 0;
+console.log(++counter);
+console.log(counter1++);
