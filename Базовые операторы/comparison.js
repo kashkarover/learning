b7cafa28d6bcb2abc886ab38c2352c18ae3bb30a
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-07 11:55:18 +0300

    if, else, ternary

---
+++ Базовые операторы/comparison.js
@@ -0,0 +1,19 @@
+console.log('A' > 'B');
+console.log('Apple' > 'apple');
+console.log('apple' > 'Apples');
+console.log('Apple' > 'Apples');
+console.log('Appre' > 'Apples');
+console.log('------------------------------------------');
+
+console.log('1' == 1);
+console.log('01' == 1);
+console.log(true == 1);
+console.log(false == 0);
+console.log('------------------------------------------');
+
+console.log(false === 0);
+console.log(true !== 1);
+console.log('------------------------------------------');
+
+console.log(null === undefined);
+console.log(null == undefined);
