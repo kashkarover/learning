9b4e4938e885e93ae970b66867616052732750b7
kashkarover <ravilek.kashkarov@yandex.ru>
2022-12-19 11:58:25 +0500

    1

--- Прототипы/Встроенные.js
+++ Прототипы/Встроенные.js
@@ -7,15 +7,39 @@
 // }
 // f.defer(1000);
 
-function f(a, b) {
-  console.log(a + b);
-}
+// function f(a, b) {
+//   console.log(a + b);
+// }
 
-Function.prototype.defer = function (ms) {
-  let f = this;
-  return function (...args) {
-    setTimeout(() => f.apply(this, args), ms);
-  };
+// Function.prototype.defer = function (ms) {
+//   let f = this;
+//   return function (...args) {
+//     setTimeout(() => f.apply(this, args), ms);
+//   };
+// };
+
+// f.defer(1000)(1, 2);
+
+//Методы прототипов
+
+// let obj = Object.create(null);
+// obj.name = 'Ravil';
+// console.log(obj)
+
+let dictionary = Object.create(null);
+
+dictionary.toString = function () {
+  console.log(Object.keys(this).join());
 };
 
-f.defer(1000)(1, 2);
+Object.defineProperty(dictionary, 'toString', { enumerable: false });
+
+dictionary.apple = 'Apple';
+dictionary.__proto__ = 'test';
+
+for (let key in dictionary) {
+  console.log(key);
+}
+
+console.log(dictionary);
+dictionary.toString();
